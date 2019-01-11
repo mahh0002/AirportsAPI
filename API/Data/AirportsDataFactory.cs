8be139b54e2cf16b8a0a00505ff379e494a613f7
Mathias Nielsen <mathiasnabenielsen@gmail.com>
2019-01-11 16:33:44 +0100

    Added DbContext

---
+++ API/Data/AirportsDataFactory.cs
@@ -0,0 +1,18 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    public class AirportsDataFactory : DbContext
+    {
+        public AirportsDataFactory(DbContextOptions<AirportsDataFactory> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Flight> Flights { get; set; }
+    }
+}
