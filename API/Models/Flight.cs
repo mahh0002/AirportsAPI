4f7895d5cad440c77850baa2cd3832164d3a5b19
Mathias Stubberup Nabe-Nielsen <mathiasnabenielsen@gmail.com>
2019-01-28 20:27:02 +0100

    Added view for index, update models

--- API/Models/Flight.cs
+++ API/Models/Flight.cs
@@ -12,9 +12,7 @@ namespace API.Models
         public int FlightId { get; set; }
         public string AircraftType { get; set; }
         public string FromLocation { get; set; }
-        public string FromAirport { get; set; }
         public string ToLocation { get; set; }
-        public string ToAirport { get; set; }
         public DateTime DepartureTime { get; set; }
         public DateTime ArrivalTime { get; set; }
 
